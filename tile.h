171c711a1f5d529b8c640d2c0d089553bf6de9e2
ZioKli <zionklinger1@gmail.com>
2019-12-17 12:37:16 -0500

    added positional information and documentation

--- tile.h
+++ tile.h
@@ -1,27 +1,36 @@
 
+#include <cstdlib>
+
 #ifndef TILE_H
 #define TILE_H
-#include <cstdlib>
-using namespace std;
 
+using namespace std;
 
 class Tile{
     public:
     ///setters
     void setSymbol(char newSymbol);
+    void setPosY(int newY);
+    void setPosX(int newX);
 
     ///getters
     char getSymbol();
+    int getYPos();
+    int getXPos();
     ///constructors
     Tile();
-    Tile(char newSymbol);
+    Tile(char newSymbol, int NewXPos, int newYPos);
 
     private:
     char symbol;
+    int xPos;
+    int yPos;
+    /// references to the tiles adjacent and diagonal to this one 
+
     /*
     *Things to add 
     * X and Y coordinates of this tile on current screen
-    * references to the tiles adjacent and diagonal to this one 
+    
     * references to all entities 
     * references to all equipment
     * references to all consumables
