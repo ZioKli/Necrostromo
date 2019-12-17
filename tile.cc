94bb51db45c011c9945a04d7292acd1bdd8c9170
ZioKli <zionklinger1@gmail.com>
2019-12-17 12:36:54 -0500

    added positional information

--- tile.cc
+++ tile.cc
@@ -3,16 +3,42 @@
 
 Tile::Tile(){
     symbol = '.';
+    xPos = 0;
+    yPos = 0;
 }
 
-Tile::Tile(char newSymbol){
+Tile::Tile(char newSymbol, int NewXPos, int newYPos){
     symbol = newSymbol;
+    xPos = NewXPos;
+    yPos = newYPos;
 }
+
 
 void Tile::setSymbol(char newSymbol){
     symbol = newSymbol;
 }
 
+void Tile::setPosY(int newY){
+    if(newY >= 0) {
+        yPos = newY;
+    }
+}
+
+void Tile::setPosX(int newX){
+        if(newX >= 0) {
+        xPos = newX;
+    }
+}
+
+
 char Tile::getSymbol(){
     return symbol;
+}
+
+int Tile::getYPos(){
+    return yPos;
+}
+
+int Tile::getXPos(){
+    return xPos;
 }
