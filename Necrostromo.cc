688b0ec69f84c0c50d26e663f152ece832a02c70
Zion <zoinsaints@hotmail.com>
2020-01-06 13:35:03 -0500

    Added generated world

--- Necrostromo.cc
+++ Necrostromo.cc
@@ -21,44 +21,47 @@ void controlPlayer(Player player);
 void movePlayer(Player &pc, int commandCode);
 
 int main() {
-    // /// initialize ncurses
-    // initscr();  /// initializes the window
-    // cbreak();///processes one key at a time with no buffer
-    // noecho(); /// prevents the input key from being echoed to the user 
-    // keypad(stdscr, TRUE); /// allows special keys, including the arrow keys, backspace, and delete, to be used.
-    // curs_set(0);
-    // int rows, columns;
-    // getmaxyx(stdscr, rows, columns);
-    
-    // bool quit = false;
-    // Board testBoard; 
-    // Tile testTile;
-    // Player pc;
-    // vector<Entity> entities;
-    // int commandCode = 0;
-    // int quitKey = 113;
-    
-    // testTile.setSymbol('.');
-    // testBoard = Board(columns, rows, testTile);
-    
-    // while(!quit) {
-    //     drawScreen(testBoard, entities, pc);
-    //     commandCode = getch();
-    //     pc.controlPlayer(commandCode);
-    
-    //     if(commandCode == quitKey){
-    //         quit = true;
-    //         nocbreak();
-    //         keypad(stdscr, false);
-    //         echo();
-    //         endwin();
-    //     }
-    //     commandCode = 0;   
-    // }
-    // return 0;
+    /// initialize ncurses
+    initscr();  /// initializes the window
+    cbreak();///processes one key at a time with no buffer
+    noecho(); /// prevents the input key from being echoed to the user 
+    keypad(stdscr, TRUE); /// allows special keys, including the arrow keys, backspace, and delete, to be used.
+    curs_set(0);
+    int rows, columns;
+    getmaxyx(stdscr, rows, columns);
+
+    bool quit = false;
+    Board testBoard; 
+    Tile testTile;
+    Player pc;
+    vector<Entity> entities;
+    int commandCode = 0;
+    int quitKey = 113;
+
     Generator gen; 
     gen.generateNoiseMap2D();
+
+    testTile.setSymbol('.');
+    testBoard = Board(gen.getNoiseMap2D());
+
+    while(!quit) {
+        drawScreen(testBoard, entities, pc);
+        commandCode = getch();
+        pc.controlPlayer(commandCode);
+
+        if(commandCode == quitKey){
+            quit = true;
+            nocbreak();
+            keypad(stdscr, false);
+            echo();
+            endwin();
+        }
+        commandCode = 0;   
+    }
+
+    
     return 0;
+
 }
 
 void drawScreen(Board  &gameBoard, vector<Entity> const &entities, Player const &pc){ 
