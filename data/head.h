6f1a67bbf12a87b2f7d862065ebcbf437e345294
Zion <zoinsaints@hotmail.com>
2020-03-18 11:23:31 -0400

    head created as a master header file

---
+++ data/head.h
@@ -0,0 +1,10 @@
+#ifndef HEAD_H
+#define HEAD_H
+#include "generator.h"
+#include "board.h"
+#include "tile.h"
+#include "entity.h"
+#include "player.h"
+#include "item.h"
+#include "dependencies/dlist.h"
+#endif
